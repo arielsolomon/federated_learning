8a8dc4a7ae35c6dc9d3ce943e89eb3cebd8df57c
Ariel Solomon <ariel.solomon@gmail.com>
2023-09-10 15:23:28 +0300

    Add files via upload

---
+++ load_cifar.py
@@ -0,0 +1,57 @@
+﻿from collections import OrderedDict
+from typing import List, Tuple
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import CIFAR10
+
+
+DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
+
+CLASSES = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
+
+BATCH_SIZE = 32
+
+
+def load_datasets():
+    # Download and transform CIFAR-10 (train and test)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
+    testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
+
+    # Split training set into 10 partitions to simulate the individual dataset
+    partition_size = len(trainset) // 1
+    lengths = [partition_size] * 1
+    datasets = random_split(trainset, lengths, torch.Generator().manual_seed(42))
+
+    # Split each partition into train/val and create DataLoader
+    trainloaders = []
+    valloaders = []
+    for ds in datasets:
+        len_val = len(ds) // 10  # 10 % validation set
+        len_train = len(ds) - len_val
+        lengths = [len_train, len_val]
+        ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(42))
+        trainloaders.append(DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True))
+        valloaders.append(DataLoader(ds_val, batch_size=BATCH_SIZE))
+    testloader = DataLoader(testset, batch_size=BATCH_SIZE)
+    return trainloaders, valloaders, testloader
+
+
+trainloaders, valloaders, testloader = load_datasets()
